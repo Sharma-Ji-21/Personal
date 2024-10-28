ec3c939ba8a50cd91bb1a2b40f7ddc8e0cb021fa
Om Sharma <justsharmaji69@gmail.com>
2024-10-28 13:48:31 +0530

    Yes Nughty

---
+++ SHHHH/Sample/LoginEmp/index.js
@@ -0,0 +1,20 @@
+const paraEl = document.querySelector(".welcome");
+const phases = [
+    "Welcome to the Login Page","Please Enter Your Details",
+];
+let phaseIndex = 0;
+let charIndex = 0;
+
+function textAni() {
+    paraEl.innerHTML = `${phases[phaseIndex].slice(0, charIndex)}|.`;
+    charIndex++;
+    if (charIndex > phases[phaseIndex].length) {
+        charIndex = 0;
+        phaseIndex++; 
+    }
+    if (phaseIndex >= phases.length) {
+        // phaseIndex = 0;
+    }
+    setTimeout(textAni, 120);
+}
+document.addEventListener("DOMContentLoaded", textAni);
